715c08b5d072c19365070f61f25de0b4555d0848
ShulamitMualem <38328264650@mby.co.il>
2025-01-31 01:50:34 +0200

    31.01.2025

--- TestsProject/UserReposetoryUnitTesting.cs
+++ TestsProject/UserReposetoryUnitTesting.cs
@@ -19,4 +19,31 @@ public class UserReposetoryUnitTesting
         var result = await userRepository.loginUser(user.Email, user.Password);
         Assert.Equal(user,result);
     }
+    [Fact]
+    public async void Login_InvalidEmailReturnsNull()
+    {
+        var user = new User { FirstName = "aa", LastName = "bb", Email = "Tz@123cvv", Password = "secure123" };
+        var users = new List<User>() { user };
+        var mockContext = new Mock<ShopApiContext>();
+        mockContext.Setup(x => x.Users).ReturnsDbSet(users);
+        var userRepository = new UserRepository(mockContext.Object);
+
+        var result = await userRepository.loginUser("wrong@email.com", user.Password);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async void Login_InvalidPasswordReturnsNull()
+    {
+        var user = new User { FirstName = "aa", LastName = "bb", Email = "Tz@123cvv", Password = "secure123" };
+        var users = new List<User>() { user };
+        var mockContext = new Mock<ShopApiContext>();
+        mockContext.Setup(x => x.Users).ReturnsDbSet(users);
+        var userRepository = new UserRepository(mockContext.Object);
+
+        var result = await userRepository.loginUser(user.Email, "wrongpassword");
+
+        Assert.Null(result);
+    }
 }
